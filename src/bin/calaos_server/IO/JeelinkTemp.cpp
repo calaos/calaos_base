141b9e55c3b331ac5b6176f50f43c36fe5aa53bc
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2013-05-28 00:01:32 +0200

    Revert "add jeelink module in lib, and add usage of this module in IO JeelinktTemp"
    This reverts commit cc9334dc5534fb61dc7012a296eaf983a1f40b7f.

--- src/bin/calaos_server/IO/JeelinkTemp.cpp
+++ src/bin/calaos_server/IO/JeelinkTemp.cpp
@@ -26,9 +26,6 @@
 #include <ListeRule.h>
 #include <IPC.h>
 
-#include <Jeelink.h>
-#include <base64.h>
-
 
 
 using namespace Calaos;
@@ -40,62 +37,17 @@ JeelinkTemp::JeelinkTemp(Params &p):
 {
         std::string tmp;
 
-	TstJeelinkSensorInfo SensorInfo;
-
-	Jeelink_prot = get_param("jeelink_prot");
         Jeelink_id = get_param("jeelink_id");
-	Jeelink_chn = get_param("jeelink_chn");
         tmp = get_param("time");
         Jeelink_args = get_param("Jeelink_args");
         time = atof(tmp.c_str());
 
-       //printf("Jeelink_ID : 0x%x, time : %3.3f\n", atoi(Jeelink_id.c_str()), time);
-	//printf("Jeelink_Chn : 0x%x\n", atoi(Jeelink_chn.c_str()));
-
-	unsigned int Id,Chn;
-	sscanf(Jeelink_id.c_str(),"%04x",(unsigned int*)&Id);
-	sscanf(Jeelink_chn.c_str(),"%04x",(unsigned int*)&Chn);
-
-	printf("\n[JeelinkTemp] ID = 0x%x, Chn=0x%x",Id,Chn);
+        printf("Jeelink_ID : %s, time : %3.3f\n", Jeelink_id.c_str(), time);
 
         if (!get_params().Exists("visible")) set_param("visible", "true");
         ListeRule::Instance().Add(this); //add this specific input to the EventLoop
 
-	
-	//tmp = get_param("id");
-
-	value = 0;
-	oldvalue = 0;
-/*	if(strcmp(tmp.c_str(),"input_2") == 0)
-	{       printf("Input ID is %s, init test to 0\n",tmp.c_str());
-		value = 0;
-	}else if(atoi(tmp.c_str()) == 3)
-	{
-		printf("Input ID is %s, init test to 30\n",tmp.c_str());
-		value = 30;
-	}
-*/
-	/* open jeelink */
-         char * device = "/dev/ttyUSB1";
-	 printf("\nJeelink:try to open %s",device); 
-	if(strcmp(Jeelink_prot.c_str(),"OSV2")==0)
-	{	
-		SensorInfo.prot = OSV2;
-		SensorInfo.SensorId = Id;
-		SensorInfo.Chn = Chn; 
-	}
-	else if(strcmp(Jeelink_prot.c_str(),"OSV3")==0)
-	{
-		SensorInfo.prot = OSV3;
-		SensorInfo.SensorId = Id;
-		SensorInfo.Chn = Chn; 
-	}
-	handle = openJeelink(device,&SensorInfo);
-	if(handle<0)
-		printf("\n[JeelinkTemp]Error Opening Jeelink");
-	
-
-
+	value = 5.0;
 
 
 
@@ -114,67 +66,19 @@ JeelinkTemp::~JeelinkTemp()
 
 void JeelinkTemp::hasChanged()
 {
-
+static int Test = 0;
 Utils::logger("input") << Priority::INFO << "JeelinkTemp::Has Changedk" << log4cpp::eol;
 
-//if(test++ > 500)
-/*if(test > 500)
+if(Test++ > 500)
 	{
 		string sig = "input ";
 		EmitSignalInput();
                 sig += get_param("id") + " ";
                 sig += Utils::url_encode(string("state:") + to_string(get_value_double()));
                 IPC::Instance().SendEvent("events", sig);
-		test = 0;
+		Test = 0;
 		value += 1.0;
-		printf("JeelinkTemp, Has Changed : temp=%3.3f; IPC is %s\n", value,sig.c_str() );
-	}
-*/
-
-	
-	if(handle>=0)
-	{
-		unsigned short size = 32;
-		unsigned char buf[32];
-		
-		if(readJeelink(handle,&size,buf)==0)
-		{
-			//extractTemp(buf);
-			
-			if(strcmp(Jeelink_prot.c_str(),"OSV2")==0)
-			{
-				int sign = (buf[6]&0x8) ? -1 : 1;
-	
-				float temp = ((buf[5]&0xF0) >> 4)*10 + (buf[5]&0xF) + (float)(((buf[4]&0xF0) >> 4) / 10.0);
-				int hum = (buf[7]&0xF) * 10 + ((buf[6]&0xF0) >> 4);
-
-				oldvalue = value;
-				value = temp*sign;
-			}
-			else if(strcmp(Jeelink_prot.c_str(),"OSV3")==0)
-			{
-				int conso = ((buf[4])<<8) + (buf[3]);
-				oldvalue = value;
-				value = conso;
-			}
-			else printf("\n[JeelinkTemp]no matching protocol");
-			
-
-
-
-						
-
-
-                        if(oldvalue != value)
-			{	string sig = "input ";
-				EmitSignalInput();
-				sig += get_param("id") + " ";
-				sig += Utils::url_encode(string("state:") + to_string(get_value_double()));
-				IPC::Instance().SendEvent("events", sig);
-				printf("JeelinkTemp, Has Changed : Value=%3.3f;\n", value );
-			}
-		}
-
+		printf("JeelinkTemp, Has Changed : temp=%3.3f\n", value);
 	}
 }
 
@@ -185,7 +89,6 @@ double JeelinkTemp::get_value_double()
 
 void JeelinkTemp::force_input_double(double v)
 {
-	printf("JeelinkTemp, Force Input\n");
         value = v;
         EmitSignalInput();
 
@@ -194,5 +97,3 @@ void JeelinkTemp::force_input_double(double v)
         sig += Utils::url_encode(string("state:") + to_string(get_value_double()));
         IPC::Instance().SendEvent("events", sig);
 }
-
-
