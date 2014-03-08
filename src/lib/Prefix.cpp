e3e8b38f774285258f88d1d455e96805007a0f85
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-03-08 12:21:25 +0100

    Add Prefix class

---
+++ src/lib/Prefix.cpp
@@ -0,0 +1,83 @@
+/******************************************************************************
+**  Copyright (c) 2007-20014, Calaos. All Rights Reserved.
+**
+**  This file is part of Calaos Home.
+**
+**  Calaos Home is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  Calaos Home is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar; if not, write to the Free Software
+**  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+**
+******************************************************************************/
+
+#include <Prefix.h>
+
+Prefix::Prefix(int argc, char **argv)
+{
+    pfx = eina_prefix_new(argv[0], NULL, "CALAOS", "calaos", NULL, 
+                             PACKAGE_BIN_DIR, PACKAGE_LIB_DIR, 
+                             PACKAGE_DATA_DIR, PACKAGE_LOCALE_DIR);
+    if (!pfx)
+        cError() << "Error in finding preffix";
+    else
+    {
+        cInfo() << "Install preffix is " << eina_prefix_get(pfx);
+        cInfo() << "Binaries directory is  " << eina_prefix_bin_get(pfx);
+        cInfo() << "Libraries directory is " << eina_prefix_lib_get(pfx);
+        cInfo() << "Data directory is " << eina_prefix_data_get(pfx);
+        cInfo() << "Locale directory is " << eina_prefix_locale_get(pfx);
+    }
+}
+
+Prefix::~Prefix()
+{
+    if (pfx)
+        eina_prefix_free(pfx);
+}
+
+string Prefix::binDirectoryGet()
+{ 
+    const char *str = eina_prefix_bin_get(pfx); 
+    // test returned string to avoir exception
+    if (str) 
+        return str; 
+    else return "";
+}
+
+string Prefix::libDirectoryGet()
+{
+    const char *str = eina_prefix_lib_get(pfx); 
+    // test returned string to avoir exception
+    if (str) 
+        return str; 
+    else return "";
+}
+
+string Prefix::dataDirectoryGet()
+{
+    const char *str = eina_prefix_data_get(pfx); 
+    // test returned string to avoir exception
+    if (str) 
+        return str; 
+    else return "";
+}
+
+string Prefix::localeDirectoryGet()
+{
+    const char *str = eina_prefix_locale_get(pfx); 
+    // test returned string to avoir exception
+    if (str) 
+        return str; 
+    else return "";
+}
+
+
---
+++ src/lib/Prefix.h
@@ -0,0 +1,40 @@
+#ifndef CPREFIX_H
+#define CPREFIX_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <Utils.h>
+#include <Eina.h>
+
+// This class is an C++ implementarion of eina_prefix
+// When the instance is created, eina try to find in whih prefix
+// Calaos has been installed. So when you call dataDirectoryGet, 
+// it's able to retrieve the path of data directory.
+// For example if you install calaos in : /opt/usr dataDirectoryGet will
+// returns /opt/usr/share/calaos as for dataDirectoryGet
+// returns /opt/usr/bin as for binDirectoryGet
+// returns /opt/usr/lib as for libDirectoryGet
+// returns /opt/usr/locale as for localeDirectoryGet
+
+class Prefix
+{
+public:
+    static Prefix &Instance(int argc, char **argv)
+    {
+        static Prefix prefix(argc, argv);
+        return prefix;
+    }
+
+    Eina_Prefix *pfx;
+    Prefix(int argc, char **argv);
+    ~Prefix();
+
+    string binDirectoryGet();
+    string libDirectoryGet();
+    string dataDirectoryGet();
+    string localeDirectoryGet();
+};
+
+#endif // CPREFIX_H
