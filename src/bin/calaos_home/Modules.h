0e032a519adb4bd9ec4b64b1fc215f93c7349080
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-03-10 16:09:10 +0100

    Uniform licence header

--- src/bin/calaos_home/Modules.h
+++ src/bin/calaos_home/Modules.h
@@ -1,14 +1,14 @@
 /******************************************************************************
- **  Copyright (c) 2006-2012, Calaos. All Rights Reserved.
+ **  Copyright (c) 2007-2014, Calaos. All Rights Reserved.
  **
- **  This file is part of Calaos Home.
+ **  This file is part of Calaos.
  **
- **  Calaos Home is free software; you can redistribute it and/or modify
+ **  Calaos is free software; you can redistribute it and/or modify
  **  it under the terms of the GNU General Public License as published by
  **  the Free Software Foundation; either version 3 of the License, or
  **  (at your option) any later version.
  **
- **  Calaos Home is distributed in the hope that it will be useful,
+ **  Calaos is distributed in the hope that it will be useful,
  **  but WITHOUT ANY WARRANTY; without even the implied warranty of
  **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  **  GNU General Public License for more details.
--- src/lib/NTPClock.cpp
+++ src/lib/NTPClock.cpp
@@ -1,15 +1,14 @@
-
 /******************************************************************************
- **  Copyright (c) 2007-2008, Calaos. All Rights Reserved.
+ **  Copyright (c) 2006-2014, Calaos. All Rights Reserved.
  **
- **  This file is part of Calaos Home.
+ **  This file is part of Calaos.
  **
- **  Calaos Home is free software; you can redistribute it and/or modify
+ **  Calaos is free software; you can redistribute it and/or modify
  **  it under the terms of the GNU General Public License as published by
  **  the Free Software Foundation; either version 3 of the License, or
  **  (at your option) any later version.
  **
- **  Calaos Home is distributed in the hope that it will be useful,
+ **  Calaos is distributed in the hope that it will be useful,
  **  but WITHOUT ANY WARRANTY; without even the implied warranty of
  **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  **  GNU General Public License for more details.
@@ -19,6 +18,7 @@
  **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  **
  ******************************************************************************/
+
 #include <NTPClock.h>
 
 using namespace std;
