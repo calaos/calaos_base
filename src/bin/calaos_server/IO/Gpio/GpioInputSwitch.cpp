6dba08c6a52497b65e35caf08e2cdbc8f1eb92c2
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-03-25 20:02:37 +0100

    Add GpioInputSwitch

---
+++ src/bin/calaos_server/IO/Gpio/GpioInputSwitch.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+ **  Copyright (c) 2006-2014, Calaos. All Rights Reserved.
+ **
+ **  This file is part of Calaos.
+ **
+ **  Calaos is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Calaos is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Foobar; if not, write to the Free Software
+ **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **
+ ******************************************************************************/
+#include <GpioInputSwitch.h>
+
+using namespace Calaos;
+
+GpioInputSwitch::GpioInputSwitch(Params &p):
+    InputSwitch(p),
+    gpioctrl(NULL)
+{
+    int gpio_nb;
+    
+    Utils::from_string(get_param("gpio"), gpio_nb);
+
+    gpioctrl = new GpioCtrl(gpio_nb);
+    gpioctrl->setDirection("out");
+
+    gpioctrl->setValueChanged([=] {
+            gpioctrl->getVal(value);
+            hasChanged();
+        });
+}
+
+GpioInputSwitch::~GpioInputSwitch()
+{
+    delete gpioctrl;
+}
+
+
