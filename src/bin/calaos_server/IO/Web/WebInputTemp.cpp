b107e78660259eb55b43c6839076907fb904cb9e
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2014-04-02 07:07:23 +0200

    Add WebInputTemp type

---
+++ src/bin/calaos_server/IO/Web/WebInputTemp.cpp
@@ -0,0 +1,58 @@
+/******************************************************************************
+ **  Copyright (c) 2006-2014, Calaos. All Rights Reserved.
+ **
+ **  This file is part of Calaos.
+ **
+ **  Calaos is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Calaos is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Foobar; if not, write to the Free Software
+ **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **
+ ******************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <ListeRule.h>
+#include <WebInputTemp.h>
+#include <WebCtrl.h>
+#include <jansson.h>
+
+using namespace Calaos;
+
+WebInputTemp::WebInputTemp(Params &p):
+    InputTemp(p)
+{
+    cInfoDom("input") << "WebInputTemp::WebInputTemp()";
+
+    // Add input to WebCtrl instance
+    WebCtrl::Instance(p).Add(readTime);
+
+    //read value when calaos_server is started
+    readValue();
+    Calaos::StartReadRules::Instance().ioRead();
+}
+
+WebInputTemp::~WebInputTemp()
+{
+    cInfoDom("input") << "WebInputTemp::~WebInputTemp()";
+}
+
+
+void WebInputTemp::readValue()
+{
+  // Read the value
+    value = WebCtrl::Instance(get_params()).getValue(get_param("path"));
+    emitChange();
+}
+
+
