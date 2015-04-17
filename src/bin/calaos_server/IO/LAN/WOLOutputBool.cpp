7c0909138aef1125bf59835f915a826e9fa85bfc
Raoul Hecky <raoul.hecky@gmail.com>
2015-04-17 15:16:45 +0200

    Add WakeOnLan output

---
+++ src/bin/calaos_server/IO/LAN/WOLOutputBool.cpp
@@ -0,0 +1,173 @@
+/******************************************************************************
+ **  Copyright (c) 2007-2015, Calaos. All Rights Reserved.
+ **
+ **  This file is part of Calaos.
+ **
+ **  Calaos is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Calaos is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Foobar; if not, write to the Free Software
+ **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **
+ ******************************************************************************/
+#include "WOLOutputBool.h"
+#include "IOFactory.h"
+#include "IPC.h"
+
+using namespace Calaos;
+
+REGISTER_OUTPUT(WOLOutputBool)
+
+Eina_Bool WOLOutputBool_con_data_written(void *data, int type, void *event)
+{
+    VAR_UNUSED(type);
+    WOLOutputBool *w = reinterpret_cast<WOLOutputBool *>(data);
+    Ecore_Con_Event_Server_Write *ev = reinterpret_cast<Ecore_Con_Event_Server_Write *>(event);
+
+    if (!ev || !w ||
+        w->udp_con != ev->server)
+        return ECORE_CALLBACK_PASS_ON;
+
+    w->data_size -= ev->size;
+    if (w->data_size <= 0)
+    {
+        w->data_size = 0;
+        ecore_con_server_del(w->udp_con);
+        w->udp_con = nullptr;
+    }
+
+    return ECORE_CALLBACK_DONE;
+}
+
+Eina_Bool WOLOutputBool_con_data_error(void *data, int type, void *event)
+{
+    VAR_UNUSED(type);
+    WOLOutputBool *w = reinterpret_cast<WOLOutputBool *>(data);
+    Ecore_Con_Event_Server_Error *ev = reinterpret_cast<Ecore_Con_Event_Server_Error *>(event);
+
+    if (!ev || !w ||
+        w->udp_con != ev->server)
+        return ECORE_CALLBACK_PASS_ON;
+
+    cDebugDom("input") << "WakeOnLan: Error, " << ev->error;
+    w->data_size = 0;
+    w->udp_con = nullptr;
+
+    return ECORE_CALLBACK_DONE;
+}
+
+WOLOutputBool::WOLOutputBool(Params &p):
+    Output(p)
+{
+    hwritten = ecore_event_handler_add(ECORE_CON_EVENT_SERVER_WRITE, WOLOutputBool_con_data_written, this);
+    herr = ecore_event_handler_add(ECORE_CON_EVENT_SERVER_ERROR, WOLOutputBool_con_data_error, this);
+}
+
+WOLOutputBool::~WOLOutputBool()
+{
+    ecore_event_handler_del(herr);
+    ecore_event_handler_del(hwritten);
+    if (udp_con)
+        ecore_con_server_del(udp_con);
+}
+
+bool WOLOutputBool::set_value(bool val)
+{
+    if (val)
+        doWakeOnLan();
+
+    return true;
+}
+
+bool WOLOutputBool::set_value(string val)
+{
+    return set_value(val == "true");
+}
+
+void WOLOutputBool::doWakeOnLan()
+{
+    //Decode MAC address
+
+    auto parseHex = [](char c)
+    {
+        int h = -1;
+        if (c >= '0' && c <= '9') h = c - '0';
+        if (c >= 'a' && c <= 'f') h = c - 'a' + 10;
+        return h;
+    };
+
+    string addr = get_param("address");
+    Utils::replace_str(addr, ":", "");
+    Utils::replace_str(addr, "-", "");
+    Utils::replace_str(addr, ".", "");
+
+    bool err = false;
+    for (uint i = 0;i < addr.length();i++)
+    {
+        char c = addr[i];
+        if (c >= '0' && c <= '9') continue;
+        if (c >= 'a' && c <= 'f') continue;
+
+        err = true;
+        break;
+    }
+
+    if (addr.length() != 12 || err)
+    {
+        cErrorDom("output") << "WakeOnLan: Wrong MAC address: " << addr;
+        return;
+    }
+
+    vector<uint8_t> address;
+    for (int i = 0;i < 12;i += 2)
+        address.push_back(parseHex(addr[i]));
+
+    vector<uint8_t> magicPacket;
+    magicPacket.reserve(6 + 6 * 16);
+
+    // First 6bytes, 0xFF
+    for (int i = 0;i < 6;i++)
+        magicPacket.push_back(0xFF);
+
+    // 16 times mac address
+    for (int i = 0;i < 16;i++)
+        magicPacket.insert(magicPacket.end(), address.begin(), address.end());
+
+    //Send packet
+    if (!udp_con)
+        udp_con = ecore_con_server_connect(ECORE_CON_REMOTE_BROADCAST,
+                                           "255.255.255.255",
+                                           BCAST_UDP_PORT,
+                                           this);
+
+    ecore_con_server_send(udp_con, magicPacket.data(), magicPacket.size());
+    data_size += magicPacket.size();
+
+    EmitSignalOutput();
+    string sig = "output ";
+    sig += get_param("id") + " " + Utils::url_encode("state:true");
+    IPC::Instance().SendEvent("events", sig);
+
+    if (timerState)
+        timerState->Reset();
+    else
+        timerState = new EcoreTimer(1.5, sigc::mem_fun(*this, &WOLOutputBool::timerTimeout));
+}
+
+void WOLOutputBool::timerTimeout()
+{
+    EmitSignalOutput();
+    string sig = "output ";
+    sig += get_param("id") + " " + Utils::url_encode("state:false");
+    IPC::Instance().SendEvent("events", sig);
+
+    DELETE_NULL(timerState);
+}
---
+++ src/bin/calaos_server/IO/LAN/WOLOutputBool.h
@@ -0,0 +1,61 @@
+/******************************************************************************
+ **  Copyright (c) 2007-2015, Calaos. All Rights Reserved.
+ **
+ **  This file is part of Calaos.
+ **
+ **  Calaos is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 3 of the License, or
+ **  (at your option) any later version.
+ **
+ **  Calaos is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with Foobar; if not, write to the Free Software
+ **  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **
+ ******************************************************************************/
+#ifndef WOLOutputBool_H
+#define WOLOutputBool_H
+
+#include "Output.h"
+#include <Ecore.h>
+#include <Ecore_Con.h>
+#include <EcoreTimer.h>
+
+using namespace Calaos;
+
+class WOLOutputBool: public Output
+{
+protected:
+    bool value = false;
+
+    Ecore_Con_Server *udp_con = nullptr;
+    Ecore_Event_Handler *hwritten = nullptr;
+    Ecore_Event_Handler *herr = nullptr;
+    int data_size = 0;
+
+    EcoreTimer *timerState = nullptr;
+
+    friend Eina_Bool WOLOutputBool_con_data_written(void *data, int type, void *event);
+    friend Eina_Bool WOLOutputBool_con_data_error(void *data, int type, void *event);
+
+    void timerTimeout();
+
+    void doWakeOnLan();
+
+public:
+    WOLOutputBool(Params &p);
+    virtual ~WOLOutputBool();
+
+    DATA_TYPE get_type() { return TBOOL; }
+
+    virtual bool set_value(bool val);
+    virtual bool get_value_bool() { return value; }
+    virtual bool set_value(string val);
+};
+
+#endif // PINGINPUTSWITCH_H
